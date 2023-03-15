8853929723aee48050619edb9f1c0598967b7982
Maksim Karandasov <max.mayewski@gmail.com>
2023-03-15 20:35:20 +0400

    Initial Commit

---
+++ HM1 Maksim Karandasov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM1 Maksim Karandasov
+//
+//  Created by Max Mayewski on 15.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
